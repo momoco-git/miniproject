3a78bb0bf31ee0e1dc1e37f1bbb09b5cc10bef89
momoco-git <109949855+momoco-git@users.noreply.github.com>
2022-09-06 14:33:09 +0900

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -6,7 +6,7 @@ const BASE_URL = "http://54.177.177.138";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Accept": "application/json",
+    "Accept": "*/*",
     "Content-Type": "application/json",
   },
   // 토큰 추가하기
